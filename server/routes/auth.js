6e7972b7eb0dd04d1ecd112a7b398a2024870790
Rendell Lasola <rendelllasola@gmail.com>
2019-10-22 23:40:05 +0800

    Added logout cookie

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,7 @@ const {
   register,
   login,
   getMe,
+  logout,
   forgotPassword,
   resetPassword,
   updateDetails,
@@ -16,6 +17,7 @@ const { protect } = require("../middleware/auth");
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe);
+router.get("/logout", logout);
 router.put("/updatedetails", protect, updateDetails);
 router.put("/updatepassword", protect, updatePassword);
 router.post("/forgotpassword", forgotPassword);
